a232b60d4311cf68addc3db5b3eef84cfd03ab73
Alex <alexcaceres93@gmail.com>
2020-03-15 21:05:58 +0100

    Added API

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const div = document.querySelector("body div");
+const input = document.querySelector("#pokeid");
+
+function showPokemon(id) {
+  const pokemonList = data => {
+    fetch(data[id].url)
+      .then(resp => resp.json())
+      .then(data => {
+        pokemon(data);
+      });
+  };
+
+  const pokemon = data => {
+    let pokediv = document.createElement("div");
+    div.innerHTML = `
+  <img src="${data.sprites.front_default}" >
+  <div class="info">
+    <span class="name">${data.name}</span>
+    <span class="id">${data.id}</span>
+  </div>
+`;
+  };
+
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+    .then(resp => resp.json())
+    .then(data => {
+      pokemonList(data.results);
+    });
+}
+document.addEventListener("keyup", event => {
+  if (event.keyCode == 13) {
+    if (input.value != "") {
+      showPokemon(input.value);
+    }
+  }
+});
